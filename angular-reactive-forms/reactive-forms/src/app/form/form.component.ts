1f1586fa9ac98d81347e43f98d797caa36544966
parthpsp1 <pspparth1998@gmail.com>
2022-07-01 23:45:38 +0530

    Updated HTML

--- angular-reactive-forms/reactive-forms/src/app/form/form.component.ts
+++ angular-reactive-forms/reactive-forms/src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { TSize } from '../t-size';
 
 @Component({
   selector: 'app-form',
@@ -8,8 +9,16 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class FormComponent implements OnInit {
 
-  constructor(private form_builder: FormBuilder) { }
+  tSizecomp = TSize;
+  t_size_array: any = [];
+  public t_sizes = Object.values(this.tSizecomp).filter(value => typeof value === 'string');
+
+
+  constructor(private form_builder: FormBuilder) {
+    this.t_size_array = Object.keys(this.tSizecomp);
+  }
   ngOnInit(): void {
+    console.log(this.tSizecomp)
   }
 
   asterisk = "*";
@@ -21,33 +30,44 @@ export class FormComponent implements OnInit {
     first_name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
     middle_name: ['', [Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
     last_name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
-    department: [''],
-    location: [''],
-    work_phone: [''],
-    salary_revision_due_on: [''],
-    date_of_joining: [''],
-    mobile_phone: [''],
-    address: [''],
-    other_email: [''],
-    tags: [''],
+    department: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
+    location: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15), Validators.pattern("[a-zA-Z]+$")]],
+    work_phone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
+    salary_revision_due_on: ['',],
+    date_of_joining: ['', [Validators.required]],
+    mobile_phone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
+    address: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+    other_email: ['', [Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")]],
+    tags: ['', [Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
     birth_date: [''],
     current_t_size: [''],
     marital_status: [''],
     blood_group: [''],
-    age: [''],
+    age: ['', [Validators.pattern("^(?:1[01][0-9]|99|1[7-9]|[2-9][0-9])$")]],
+    job_description: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+    about_me: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+    ask_me_about_expertise: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+    present_address: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+    residence_phone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
+  });
+
+
+  work_exp_table = this.form_builder.group({
+    previous_company: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15), Validators.pattern("[a-zA-Z]+$")]],
+    job_title: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15), Validators.pattern("[a-zA-Z]+$")]],
+    form_date: ['', [Validators.required]],
+    to_date: ['', [Validators.required]],
     job_description: [''],
-    about_me: [''],
-    ask_me_about_expertise: [''],
-    present_address: [''],
-    residence_phone: [''],
-  })
+    relevance: ['']
+  });
+
 
   submitForm() {
     alert("Submit Working");
     console.warn(this.form.value);
   }
 
-  get p() {
+  get details_form() {
     return this.form.controls;
   }
 }
