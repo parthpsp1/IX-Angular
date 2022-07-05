149ab933e8183d617eb115c89f3055126fb6b891
parthpsp1 <pspparth1998@gmail.com>
2022-07-05 11:01:27 +0530

    Uploading Reactive Forms assignment

--- angular-reactive-forms/reactive-forms/src/app/app.module.ts
+++ angular-reactive-forms/reactive-forms/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { AgeDirective } from './core/age.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    AgeDirective
   ],
   imports: [
     BrowserModule,
--- angular-reactive-forms/reactive-forms/src/app/form/form.component.ts
+++ angular-reactive-forms/reactive-forms/src/app/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BloodGroup } from '../blood-group';
+import { MaritalStatus } from '../marital-status';
 import { TSize } from '../t-size';
 
 @Component({
@@ -10,61 +12,76 @@ import { TSize } from '../t-size';
 export class FormComponent implements OnInit {
 
   tSizecomp = TSize;
-  t_size_array: any = [];
-  public t_sizes = Object.values(this.tSizecomp).filter(value => typeof value === 'string');
-
-
-  constructor(private form_builder: FormBuilder) {
-    this.t_size_array = Object.keys(this.tSizecomp);
-  }
-  ngOnInit(): void {
-    console.log(this.tSizecomp)
-  }
-
+  blood_groups = BloodGroup;
+  marital_status = MaritalStatus;
+  current_year = new Date();
+  user_age: number = 0;
   asterisk = "*";
   form_title = "Details Form";
   form_data: string = '';
+  date = new Date().toISOString().slice(0, 10);
+  form: FormGroup = new FormGroup({});
 
-  form = this.form_builder.group({
-    employee_number: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(5), Validators.pattern("^[0-9]*$")]],
-    first_name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
-    middle_name: ['', [Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
-    last_name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
-    department: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
-    location: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15), Validators.pattern("[a-zA-Z]+$")]],
-    work_phone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
-    salary_revision_due_on: ['',],
-    date_of_joining: ['', [Validators.required]],
-    mobile_phone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
-    address: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
-    other_email: ['', [Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")]],
-    tags: ['', [Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
-    birth_date: [''],
-    current_t_size: [''],
-    marital_status: [''],
-    blood_group: [''],
-    age: ['', [Validators.pattern("^(?:1[01][0-9]|99|1[7-9]|[2-9][0-9])$")]],
-    job_description: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
-    about_me: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
-    ask_me_about_expertise: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
-    present_address: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
-    residence_phone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
-  });
+  constructor(private form_builder: FormBuilder) {
+  }
+  ngOnInit(): void {
+    this.form = this.form_builder.group({
+      employee_number: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(5), Validators.pattern("^[0-9]*$")]],
+      first_name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
+      middle_name: ['', [Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
+      last_name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z]+$")]],
+      department: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20), Validators.pattern("[a-zA-Z0-9 ]+$")]],
+      location: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20), Validators.pattern("[a-zA-Z0-9 ]+$")]],
+      work_phone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
+      salary_revision_due_on: ['',],
+      date_of_joining: ['', [Validators.required]],
+      mobile_phone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
+      address: ['', [Validators.minLength(2), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      other_email: ['', [Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")]],
+      tags: ['', [Validators.minLength(2), Validators.maxLength(10), Validators.pattern("[a-zA-Z ,_.@]+$")]],
+      birth_date: [''],
+      current_t_size: [''],
+      marital_status: [''],
+      blood_group: [''],
+      age: ['', [Validators.maxLength(2), Validators.pattern("^(18?[1-9]|[1-9][0-9]|[1][1-9][1-9]|99)$")]],
+      job_description: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      about_me: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      ask_me_about_expertise: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      present_address: ['', [Validators.minLength(10), Validators.maxLength(99), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      residence_phone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
+      work_experiance: this.form_builder.array([])
+    });
+  }
 
 
-  work_exp_table = this.form_builder.group({
-    previous_company: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15), Validators.pattern("[a-zA-Z]+$")]],
-    job_title: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15), Validators.pattern("[a-zA-Z]+$")]],
-    form_date: ['', [Validators.required]],
-    to_date: ['', [Validators.required]],
-    job_description: [''],
-    relevance: ['']
-  });
 
+
+
+  get addFields() {
+    return this.form.controls["work_experiance"] as FormArray;
+  }
+
+  addRow() {
+    this.addFields.push(this.form_builder.group({
+      previous_company: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15), Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      job_title: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15), Validators.pattern("[a-zA-Z]+$")]],
+      from_date: ['', [Validators.required]],
+      to_date: ['', [Validators.required]],
+      job_description: ['', [Validators.pattern("^[a-zA-Z_.-]*$")]],
+      relevance: ['']
+    }));
+  }
+
+  calculateAge() {
+    let dob = new Date(this.form.value.birth_date);
+    let year = dob.getFullYear();
+    let current_year_value = this.current_year.getFullYear()
+    this.user_age = current_year_value - year;
+  }
 
   submitForm() {
-    alert("Submit Working");
-    console.warn(this.form.value);
+    this.form.value.age = this.user_age;
+    console.log(this.form.value);
   }
 
   get details_form() {
